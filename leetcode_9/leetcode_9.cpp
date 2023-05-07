b1391a027f7406c58be17dedb633ce348d22d3bf
Дмитрий Плутицкий <phoenixo_o@mail.ru>
2023-05-08 00:34:31 +1000

    Добавьте файлы проекта.

---
+++ leetcode_9/leetcode_9.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int store_val = x;
+        long reverse_x = 0;
+        short rem = 0;
+        while (x) {
+            rem = x % 10;
+            reverse_x = reverse_x * 10 + rem;
+            x /= 10;
+        }
+        return store_val == reverse_x;
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout << s.isPalindrome(10);
+    std::cout << s.isPalindrome(0);
+    std::cout << s.isPalindrome(424);
+    std::cout << s.isPalindrome(124);
+}
